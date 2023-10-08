68bbe87300f4b5137c032ad2bb312a03679753a5
jjgouveia <gouvik.dev@gmail.com>
2023-10-07 23:59:22 -0300

    feat: Botão para fazer o download do JSON proveniente do processamento do vídeo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,8 +8,37 @@
         processData: false,
         contentType: false,
         success: function (data) {
+            dataJSON = data;
             var jsonString = JSON.stringify(data, null, 4);
             $('#jsonDisplay').text(jsonString);
         }
     });
 });
+
+$('#jsonDisplay').on('DOMSubtreeModified', function () {
+    var jsonContent = $('#jsonDisplay').text().trim();
+    if (jsonContent === '') {
+        $('#downloadLink').hide();
+    } else {
+        $('#downloadLink').show();
+    }
+});
+
+$('#downloadLink').click(function () {
+    if ($('#downloadLink').is(':visible')) {
+        let jsonData = $('#jsonDisplay').text();
+        var blob = new Blob([jsonData], { type: "application/json" });
+        var url = URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = `dados_do_video_${dataJSON.title}.json`;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    } else {
+        return false;
+    }
+});
+
+
+
